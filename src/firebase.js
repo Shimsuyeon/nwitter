b359fa4c2d3f6484a427c822b74c2107cf341b3d
Shimsuyeon <suyeonshim@gm.gist.ac.kr>
2022-12-27 22:08:25 +0900

    1.0 Initialization

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC66zO2gzqSWyTsBehm_2rOPK3758wd3C8",
+    authDomain: "nwitter-fc115.firebaseapp.com",
+    projectId: "nwitter-fc115",
+    storageBucket: "nwitter-fc115.appspot.com",
+    messagingSenderId: "381424800692",
+    appId: "1:381424800692:web:9081156c2dc694bccd58e1",
+    measurementId: "G-2CWGHGLM6M"
+};
+
+export default firebase.initializeApp(firebaseConfig);
