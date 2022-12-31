fd71701beb81ef695d1e5f1e3bac98881d9aa62c
Shimsuyeon <suyeonshim@gm.gist.ac.kr>
2022-12-31 11:21:52 +0900

    2.6 Logout

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,31 +3,38 @@ import {HashRouter as Router} from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
 import { useEffect, useState } from "react";
-
+import Navigation from "components/Navigation";
+import Profile from"routes/Profile";
 import {
     Route,
     Switch,
   } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 const AppRouter = ({isLoggedIn})=>{
     //const [isLoggedIn, setIsLoggedIn] = useState(true);
 
     return (
         <Router>
-            <Switch>
-                {isLoggedIn ? (
-                <>  
+          {isLoggedIn && <Navigation />}
+          <Switch>
+            {isLoggedIn ? (
+              <>
                 <Route exact path="/">
-                    <Home/>
+                  <Home />
                 </Route>
-                </> 
-                ): (
+                <Route exact path="/profile">
+                  <Profile />
+                </Route>
+              </>
+            ) : (
+                <>
                 <Route exact path="/">
-                    <Auth/>
+                  <Auth />
                 </Route>
-                )}
-            </Switch>
+              </>
+            )}
+          </Switch>
         </Router>
-    );
-};
-
+      );
+    };
 export default AppRouter;
